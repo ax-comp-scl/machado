5cf77ccdb8c4f8cecb0233d7732bfc9517c1f01f
Matheus <mgusmao.dev@gmail.com>
2025-05-29 21:31:21 -0300

    feat: criar serializer específico para o FASTA

--- machado/api/serializers/load.py
+++ machado/api/serializers/load.py
@@ -15,7 +15,18 @@ class FileSerializer(serializers.Serializer):
 
 class LoadPublicationSerializer(serializers.Serializer):
     file = serializers.FileField()
-    cpu = serializers.IntegerField(required=False, min_value=1, default=1)
+    cpu = serializers.IntegerField(required=False, min_value=1, default=1, help_text="Number of threads")
+
+
+class FastaSerializer(serializers.Serializer):
+    file = serializers.FileField()
+    organism = serializers.CharField(required=True, help_text="Species name (eg. Homo sapiens, Mus musculus)")
+    soterm = serializers.CharField(required=True, help_text="SO Sequence Ontology Term (eg. chromosome, assembly)")
+    description = serializers.CharField(required=False, help_text="Description")
+    url = serializers.CharField(required=False, help_text="URL")
+    doi = serializers.CharField(required=False, help_text="DOI of a reference stored using load_publication (eg. 10.1111/s12122-012-1313-4)")
+    nosequence = serializers.CharField(required=False, help_text="Don't load the sequences")
+    cpu = serializers.IntegerField(required=False, min_value=1, default=1, help_text="Number of threads")
 
 class OrganismSerializer(serializers.Serializer):
     """Organism serializer."""
