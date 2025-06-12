4592ef67118bf50acc2b02c676ee46eb5fda3178
vinicius-matos <vinicius.matos@aluno.ifsp.edu.br>
2025-06-12 21:12:18 +0100

    feat(views/load): criar endpoint para remoção de uma carga de 'GFF'

--- machado/api/views/load.py
+++ machado/api/views/load.py
@@ -1070,3 +1070,34 @@ class GFFViewSet(viewsets.GenericViewSet):
             },
             status=status.HTTP_200_OK,
         )
+    
+    def destroy(self, request):
+        """Handle the DELETE request for loading gff."""
+        file = request.data.get("file")
+
+        if not file:
+            return Response(
+                {"error": "File is a required field."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        thread = Thread(
+            target=call_command,
+            args=("remove_file",),
+            kwargs=(
+                {
+                    "file": file
+                }
+            ),
+            daemon=True
+        )
+
+        thread.start()
+
+        return Response(
+            {
+                "status": "Submited successfully",
+                "call_command": "remove_file",
+            },
+            status=status.HTTP_204_NO_CONTENT,
+        )
