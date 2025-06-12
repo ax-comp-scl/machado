f02ee27e7bd42cd693b75e5e973f1db83e130f93
abelBaes <abel.baes77@gmail.com>
2025-06-12 19:45:23 -0400

    removido load/gff duplicado

--- machado/api/urls.py
+++ machado/api/urls.py
@@ -156,12 +156,6 @@ router.register(
     basename='load_feature_publication'
 )
 
-router.register(
-    r"load/gff",
-    loadViews.GFFViewSet,
-    basename="load_gff"
-)
-
 # history
 
 router.register(r"history", readViews.HistoryListViewSet, basename="loads_history")
